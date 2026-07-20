6f6a61fa37c84e0f0f9369a717b35dc9cfb369d2
lohithrajini <lohith282006@gmail.com>
2026-07-20 22:38:57 +0530

    Time: 2 ms (70.94%), Space: 42.9 MB (63.93%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -2,13 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int fillCups(int[] amount) {
-        int time = 0;
-        while (amount[0] + amount[1] + amount[2] > 0) {
-            Arrays.sort(amount);
-            amount[2]--;
-            if (amount[1] > 0) amount[1]--;
-            time++;
-        }
-        return time;
+        Arrays.sort(amount);
+
+        int total = amount[0] + amount[1] + amount[2];
+        int largest = amount[2];
+
+        return Math.max(largest, (total + 1) / 2);
     }
 }
