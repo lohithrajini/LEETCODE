dba940e44ba7a21fe7b9c6c57c43cdfdd59fc63d
lohithrajini <lohith282006@gmail.com>
2026-07-30 22:09:27 +0530

    Time: 1 ms (75%), Space: 42.1 MB (95.1%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
@@ -1,7 +1,9 @@
 class Solution {
     public int minChanges(int n, int k) { {
-    if ((n & k) != k) return -1;
+    if ((n & k) != k) 
+     return -1;
     return Integer.bitCount(n ^ k);
+   
 }
     }
 }
